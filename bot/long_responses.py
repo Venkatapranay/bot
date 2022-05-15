779599f0dc36a41e78440492a10c2d41a771312b
Venkatapranay <100606733+Venkatapranay@users.noreply.github.com>
2022-05-15 10:40:41 +0530

    Add files via upload

---
+++ bot/long_responses.py
@@ -0,0 +1,6 @@
+import random
+r_eating="i don't eat ,i am a bot!"
+
+def unknown():
+    response=['could you please rephrase that?',"...","what does that mean?"][random.randrange(3)]
+    return response
